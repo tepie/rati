6d0f5bd9f96256c91e3beaf3e21bcc67112823b5
tepietrondi@yahoo.com <tepietrondi@yahoo.com@f95b2e77-e138-0410-83cc-19b3da825df8>
2007-08-31 03:39:52 +0000

    database changes, renamed, changed user names

--- trunk/sql/database_schema_myisam.sql
+++ trunk/sql/database_schema_myisam.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 -- 
 -- Host: localhost
--- Generation Time: Aug 22, 2007 at 09:45 AM
+-- Generation Time: Aug 30, 2007 at 08:41 PM
 -- Server version: 5.0.37
 -- PHP Version: 5.2.2
 
@@ -13,8 +13,11 @@ SET AUTOCOMMIT=0;
 START TRANSACTION;
 
 -- 
--- Database: `metawarehouse`
+-- Database: `rati`
 -- 
+DROP DATABASE `rati`;
+CREATE DATABASE `rati` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_cs;
+USE `rati`;
 
 -- --------------------------------------------------------
 
@@ -79,6 +82,7 @@ CREATE TABLE IF NOT EXISTS `search_index` (
   `COMBINED_ATTRIBUTES` text collate latin1_general_cs NOT NULL COMMENT 'Combined attributes of the object',
   `RANK` bigint(20) unsigned zerofill NOT NULL default '00000000000000000000' COMMENT 'Object Rank',
   `WEIGHT` bigint(20) unsigned zerofill NOT NULL default '00000000000000000000',
+  `PERSPECTIVE` text collate latin1_general_cs COMMENT 'Perspective Name',
   PRIMARY KEY  (`ID`),
   UNIQUE KEY `OBJECT_NAME_UNIQUE` (`OBJECT_NAME`(512)),
   KEY `RANK` (`RANK`),
@@ -86,6 +90,22 @@ CREATE TABLE IF NOT EXISTS `search_index` (
   KEY `WEIGHT` (`WEIGHT`),
   FULLTEXT KEY `COMBINED_ATTRIBUTES_TEXT` (`COMBINED_ATTRIBUTES`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs COMMENT='Search Ranking';
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `usage_assoc_ip_object`
+-- 
+
+DROP TABLE IF EXISTS `usage_assoc_ip_object`;
+CREATE TABLE IF NOT EXISTS `usage_assoc_ip_object` (
+  `ID` bigint(20) unsigned zerofill NOT NULL auto_increment COMMENT 'Primary ID',
+  `IP_ID` bigint(20) unsigned zerofill NOT NULL COMMENT 'IP ID',
+  `OBJECT_ID` bigint(20) unsigned zerofill NOT NULL COMMENT 'Object ID',
+  PRIMARY KEY  (`ID`),
+  KEY `IP_ID` (`IP_ID`,`OBJECT_ID`),
+  KEY `OBJECT_ID` (`OBJECT_ID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs COMMENT='Associate IP addresses with objects visited';
 
 -- --------------------------------------------------------
 
@@ -99,8 +119,8 @@ CREATE TABLE IF NOT EXISTS `usage_assoc_ip_search` (
   `IP_ID` bigint(20) unsigned zerofill NOT NULL COMMENT 'Usage ID Address Key',
   `SEARCH_ID` bigint(20) unsigned zerofill NOT NULL COMMENT 'Usage Search Text Key',
   PRIMARY KEY  (`ID`),
-  UNIQUE KEY `UNIQUE_IP_ID` (`IP_ID`),
-  UNIQUE KEY `UNIQUE_SEARCH_ID` (`SEARCH_ID`)
+  KEY `INDEX_IP_ID` (`IP_ID`),
+  KEY `INDEX_SEARCH_ID` (`SEARCH_ID`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs COMMENT='Associate the IP addresses to search strings';
 
 -- --------------------------------------------------------
@@ -131,19 +151,15 @@ CREATE TABLE IF NOT EXISTS `usage_search_log` (
   UNIQUE KEY `SEARCH_TEXT_UNIQUE` (`SEARCH_TEXT`(1000))
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs COMMENT='Usage of searches being used';
 
+
+DROP USER rati@localhost;
+DROP USER ratiimport@localhost;
+CREATE USER rati@localhost IDENTIFIED BY 'rati';
+CREATE USER ratiimport@localhost IDENTIFIED BY 'ratiimport';
+GRANT USAGE ON *.* TO rati@localhost IDENTIFIED BY 'rati';
+GRANT USAGE ON *.* TO rati@localhost IDENTIFIED BY 'ratiimport';
+GRANT SELECT ON `rati` . * TO rati@localhost;
+GRANT SELECT , INSERT , UPDATE , DELETE ON `rati` . * TO ratiimport@localhost;
 COMMIT;
 
--- 
--- Setup the user for this application
--- 
-
-START TRANSACTION;
-DROP USER graphviz@localhost;
-DROP USER ratiimport@localhost;
-CREATE USER graphviz@localhost IDENTIFIED BY 'graphviz';
-CREATE USER ratiimport@localhost IDENTIFIED BY 'ratiimport';
-GRANT USAGE ON *.* TO graphviz@localhost IDENTIFIED BY 'graphviz';
-GRANT USAGE ON *.* TO graphviz@localhost IDENTIFIED BY 'ratiimport';
-GRANT SELECT ON `metawarehouse` . * TO graphviz@localhost;
-GRANT SELECT , INSERT , UPDATE , DELETE ON `metawarehouse` . * TO ratiimport@localhost;
 COMMIT;
