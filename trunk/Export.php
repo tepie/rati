bc3733eb190c2103b6c82962c8ff24403f2744cd
tepietrondi@yahoo.com <tepietrondi@yahoo.com@f95b2e77-e138-0410-83cc-19b3da825df8>
2007-09-07 20:55:31 +0000

    export rewrite, cleanup

--- trunk/Export.php
+++ trunk/Export.php
@@ -9,30 +9,27 @@
 	include_once('Include\\HtmlCommon.php');
 	
 	$type_param 			= "type";
-	$type_default_export 	= "xml";
-	$type_dict 				= array( "xml" => "text/xml", "dot" => "text/plain", "img" => "image/png", "html" => "text/html");
-	
-	function howto_export(){
-		
-	}
+	$type_default_export 	= "dot";
+	//$type_dict 				= array( "xml" => "text/xml", "dot" => "text/plain", "img" => "image/png", "html" => "text/html");
+	$type_dict 				= array("dot" 	=> "text/plain",
+								"html"		=>	"text/html",
+								"img" 		=> 	"image/png", 
+								"legend" 	=> "text/html",
+								"attributes" => "text/html");
 	
 	function acceptable_type($type){
 		global $type_dict;
-		
 		foreach($type_dict as $accepted => $header){
 			if($type == $accepted) return true;
 		}
-		
 		return false;
 	}
 	
 	function select_header($type){
 		global $type_dict;
-		
 		foreach($type_dict as $accepted => $header){
 			if($type == $accepted) return $header;
 		}
-		
 		return false;
 	}
 	
@@ -53,133 +50,103 @@
 		$export_type = "$type_default_export";
 	}
 	
+	if(isset($_GET["$url_rest_node_param"])){
+		$node_name 	= $_GET["$url_rest_node_param"];
+	} else {
+		header('Location: Index.php');
+	}
+	
 	if(!acceptable_type($export_type)){
-		header("Content-type: ". $type_dict[$type_default_export]);
-		echo "<rati><export>Unsupported Export Type! xml,dot, and img are the options!</export></rati>";
+		echo "Unsupported Export Type!";
 		exit(-1);
 	}
 	
 	$header = select_header($export_type);
+	
 	commonSessionSetup();
-	header("Content-type: $header");
-		
-	// Check the URL for a "node" parameter and set the $node_name
-	if(isset($_GET["$url_rest_node_param"])){
-		/** Setup the database connection, provide the host, username and password */	
-		/** Set the node name to be used throughout this navigation */
-		$node_name = $_GET["$url_rest_node_param"];
-		
-		$query_runner 	= new QueryRunner();
-		$temp_graph_direction = $_SESSION["$url_rest_custom_image_graph_direction"];
-		$temp_arrow_direction = $_SESSION["$url_rest_custom_image_arrow_direction"];
-		$g = new GraphObject($query_runner,true,true,1,$temp_graph_direction,$temp_arrow_direction);
-		$g->walk($node_name);
-		
-		if($_SESSION[$url_rest_custom_image_font_size] == "L"){
-		/** The graphviz string LARGE */
-			$temp_graph =  $g->getGraphvizSring($fontsize="14");
-		} else {
-		/** The graphviz string */
-			$temp_graph 	=  $g->getGraphvizSring();
+	commonValidationCustomizationValues();	
+	
+	$query_runner 			= new QueryRunner();
+	$utility 				= new UtilityObject();
+	$temp_graph_direction 	= $_SESSION["$url_rest_custom_image_graph_direction"];
+	$temp_arrow_direction 	= $_SESSION["$url_rest_custom_image_arrow_direction"];
+	$temp_graph_levels		= $_SESSION["$url_rest_custom_image_graph_levels"] + 0;
+	$g 						= new GraphObject($query_runner,
+								true,true,$temp_graph_levels,
+								$temp_graph_direction,$temp_arrow_direction);
+	$g->walk($node_name);
+	
+	$temp_font_size = commonGraphvizFontSize();
+	if($temp_font_size == null){
+		$graph_string = $g->getGraphvizSring();
+	} else {
+		$graph_string = $g->getGraphvizString($fontsize="$temp_font_size");
+	}
+	
+	if($export_type == "img" or $export_type == "html"){
+		$result_setup 	= $utility->setupFileDirectories();
+		if(!$result_setup){
+			echo "Problem creating needed directories to store files, check permissions.";
+			exit(-1);
 		}
 		
-		if($export_type == "xml"){
-			echo $g->getExportXml();
-		} elseif ($export_type == "dot"){
-			echo $temp_graph;
-		} elseif ($export_type == "img"){
-			$graph_string 	= $temp_graph;
-			$utility 		= new UtilityObject();
-			$result_setup 	= $utility->setupFileDirectories();
+		$escaped 		= escapeshellarg($graph_string);
+		$checksum 		= md5($graph_string);
 		
-			if(!$result_setup){
-				echo "Problem creating needed directories to store files, check permissions.";
-				exit(-1);
-			}
-			
-			$escaped 		= escapeshellarg($graph_string);
-			$checksum 		= md5($graph_string);
-			$dot_file 		= "$directory_dot_graph" . "$filesystem_path_separator" . "$checksum";
-			$img_file 		= "$directory_dot_img" . "$filesystem_path_separator" . "$checksum" . "." . "$graph_default_image_extension";
-			
-			if($utility->checkFile($dot_file,$filesystem_age_time)){
-				/** File Handle */
-				$handle 	= fopen($dot_file,"w+");
-				fwrite($handle,$graph_string);
-				fclose($handle);
-			} 
-			
+		$dot_file 		= "$directory_dot_graph" . "$filesystem_path_separator" . "$checksum";
+		
+		if($utility->checkFile($dot_file,$filesystem_age_time)){
+			/** File Handle */
+			$handle 	= fopen($dot_file,"w+");
+			fwrite($handle,$graph_string);
+			fclose($handle);
+		} 
+		
+		if($export_type == "html"){
+			$img_file 	= "$directory_dot_img" . "$filesystem_path_separator" . "$checksum" . "." . "$graph_default_image_extension";
+			$map_file 	= "$directory_dot_map" . "$filesystem_path_separator" . "$checksum" . "." . "$graph_default_map_extension";	
+			$img_url 	= "$directory_dot_img" . "$url_path_separator" . "$checksum" . "." . "$graph_default_image_extension";
+		}
+		
+		if($export_type == "img"){
 			$mapCmd	= "$command_executable_dot -T$graph_default_image_format $dot_file";
-			//echo $mapCmd;
+			header("Content-type: $header");
 			passthru($mapCmd,$ret);
-			
-		} elseif($export_type == "html"){
-			$graph_string 	= $g->getGraphvizSring();
-			$utility 		= new UtilityObject();
-			$result_setup = $utility->setupFileDirectories();
-			
-			/** Generate a MD5 checksum against the graph graphviz string */
-			$checksum 	= md5($graph_string);
-				//echo $checksum."<br />";
-				/** Escape the checksum value to be used in the shell */
-			$escaped 	= escapeshellarg($checksum);
-			
-			if(!$result_setup){
-				echo "Problem creating needed directories to store files, check permissions.";
-				exit(-1);
-			}
-			
-			/** The attributes of the root node to show */
-			$rootAttributes = $g->getRootNodeAttributes();
-			$rootCategory 	= $g->getRootCategory();
-			
-			/** The output file for the dot graph */
-			$dot_file = "$directory_dot_graph" . "$filesystem_path_separator" . "$checksum";
-			/** The output file for the map file */
-			$map_file = "$directory_dot_map" . "$filesystem_path_separator" . "$checksum" . "." . "$graph_default_map_extension";
-			/** The image output file */
-			$img_file = "$directory_dot_img" . "$filesystem_path_separator" . "$checksum" . "." . "$graph_default_image_extension";
-			/** The URL to the image */
-			$img_url = "$directory_dot_img" . "$url_path_separator" . "$checksum" . "." . "$graph_default_image_extension";
-			
-			if(!$utility->checkFile($dot_file,$filesystem_age_time)){
-				/** File Handle */
-				$handle 	= fopen($dot_file,"w+");
-				fwrite($handle,$graph_string);
-				fclose($handle);
-			} 
-			
+		} else if($export_type == "html"){
 			if(!$utility->checkFile("$map_file",$filesystem_age_time)){
 				if(!$utility->checkFile("$img_file",$filesystem_age_time)){
 					$mapCmd	= "$command_executable_dot -Tcmap -o$map_file -T$graph_default_image_format -o$img_file $dot_file";
-					exec($mapCmd,$output,$ret);
+					exec($mapCmd,$output,$ret);			
 				}
 			} 
-
-			/** Read the contents of the map file */
-			$map_contents 	= file_get_contents("$map_file");
-			/** Clean the map file slashes */
-			$cleanMap 		= ereg_replace('\\\"','"',$map_contents);
 			
-			$html = "<table width=\"99%\">";
-			$html = $html . "<tr><td>";
-			$html = $html . "<img src=\"$img_url\" alt=\"Model\" ";
-			$html = $html . "class=\"model\" usemap=\"#$img_url\" border=\"0\">\n";
-			$html = $html . "<map name=\"$img_url\">\n";
-			$html = $html . $cleanMap;
-			$html = $html . "</map>\n";
-			$html = $html . "</td><td width=\"20%\" style=\"vertical-align:top;\">";
-			//echo "<div id=\"scroll-legend\" style=\"position:absolute;\" >";
-			$html = $html . createNodeColorLegendTable($rootCategory);
-			//echo "</div>";
-			$html = $html . "</td></tr></table>";
-			echo $html ;
-		} else {
-			echo $g->getExportXml();
+			/** Read the contents of the map file */
+			$handle 	= fopen("$map_file","rb");
+			$cleanMap 	= "";
+			while (!feof($handle)) {
+			  $cleanMap .= ereg_replace('\\\"','"',fread($handle, 8192));
+			}
+			fclose($handle);
+			header("Content-type: $header");
+			echo "<img src=\"$img_url\" alt=\"Model\" ";
+			echo "class=\"model\" usemap=\"#$img_url\" border=\"0\"></center>\n";
+			echo "<map name=\"$img_url\">\n";
+			echo $cleanMap;
+			echo "</map>";			
 		}
+		
+	} else if($export_type == "dot"){
+		header("Content-type: $header");
+		echo $graph_string;
+	} else if($export_type == "legend"){
+		header("Content-type: $header");
+		echo createNodeColorLegendTable($g->getRootCategory());
+	} else if($export_type = "attributes"){
+		header("Content-type: $header");
+		echo createAttributeTableHtml($g->getRootNodeAttributes());
 	} else {
-		echo "<rati><export>Error, no node selected</export></rati>";
+		
 	}
-	
+
 	if($db_connection->getDbLink() and $x){ $db_connection->closeLink();} 
 ?>
