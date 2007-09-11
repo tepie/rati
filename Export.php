a2646dc4d830e6fbc60b9d9a3c6c53120eb83a36
tepietrondi@yahoo.com <tepietrondi@yahoo.com@f95b2e77-e138-0410-83cc-19b3da825df8>
2007-09-11 14:22:50 +0000

    exporting and object rework

--- Export.php
+++ Export.php
@@ -11,7 +11,6 @@
 	
 	$type_param 			= "type";
 	$type_default_export 	= "dot";
-	//$type_dict 				= array( "xml" => "text/xml", "dot" => "text/plain", "img" => "image/png", "html" => "text/html");
 	$type_dict 				= array("dot" 	=> "text/plain",
 								"html"		=>	"text/html",
 								"img" 		=> 	"image/png", 
@@ -24,9 +23,7 @@
 	
 
 	function detect_is_web_export(){
-		//global $argc;
 		global $argv;
-		//echo $argc."\n";
 		if($argv[0] == "Export.php"){
 			return False;
 		} else {
@@ -62,7 +59,7 @@
 	}
 	
 	$export_is_web_based = detect_is_web_export();
-	//print_r($argv);
+	
 	if($export_is_web_based){
 		if(isset($_GET["$type_param"])){
 			$export_type = $_GET["$type_param"];
@@ -76,8 +73,6 @@
 			header('Location: Index.php');
 		} 
 	} else {
-		//echo "This is a command line export\n";
-		//echo $argc;
 		if($argc != 6){
 			echo "Wrong number of arguments.\n";
 			echo "usage: " . $argv[0] . " <export type> <node name> <graph direction> <image size> <graph levels>";
@@ -86,7 +81,6 @@
 			$export_type 	= $argv[1];
 			$node_name 		= $argv[2];
 			
-			//$_GET["$url_rest_custom_image_arrow_direction"] = $argv[3];
 			$_GET["$url_rest_custom_image_graph_direction"]	= $argv[3];
 			$_GET["$url_rest_custom_image_font_size"]		= $argv[4];
 			$_GET["$url_rest_custom_image_graph_levels"]	= $argv[5];
@@ -107,7 +101,6 @@
 	$query_runner 			= new QueryRunner();
 	$utility 				= new UtilityObject();
 	$temp_graph_direction 	= $_SESSION["$url_rest_custom_image_graph_direction"];
-	$temp_arrow_direction 	= $_SESSION["$url_rest_custom_image_arrow_direction"];
 	$temp_graph_levels		= $_SESSION["$url_rest_custom_image_graph_levels"] + 0;
 	
 	if($temp_graph_direction == "CIRCO"){
@@ -117,16 +110,13 @@
 	if($export_type == "img" or $export_type == "html" or $export_type == "dot"){
 		$g 	= new GraphObject($query_runner,
 				true,true,$temp_graph_levels,
-				$temp_graph_direction,$temp_arrow_direction);
-								
-		$g->walk($node_name);
+				$temp_graph_direction);
 		
 		$temp_font_size = commonGraphvizFontSize();
 		if($temp_font_size == null){
-			$graph_string = $g->getGraphvizSring();
+			$temp_graphviz_filename = $g->walk($node_name);
 		} else {
-			$graph_string = $g->getGraphvizSring($fontsize="$temp_font_size");
-			//$graph_string = $g->getGraphvizSring();
+			$temp_graphviz_filename = $g->walk($node_name,$fontsize=$temp_font_size);
 		}
 	} else if($export_type == "legend" or $export_type == "attributes"){
 		$node = new NodeObject($query_runner,$node_name,$mysql_database_neighbor_limit);
@@ -139,22 +129,7 @@
 			exit(-1);
 		}
 		
-		$escaped 		= escapeshellarg($graph_string);
-		$checksum 		= md5($graph_string);
-		
-		$dot_file 		= "$directory_dot_graph" . "$filesystem_path_separator" . "$checksum";
-		
-		/*if($url_rest_custom_image_graph_direction == "CIRCO"){
-			echo "Changing command line...";
-			$command_executable_dot = "circo";
-		}*/
-		
-		if(!file_exists($dot_file) or $utility->checkFile($dot_file,$filesystem_age_time)){
-			/** File Handle */
-			$handle 	= fopen($dot_file,"w+");
-			fwrite($handle,$graph_string);
-			fclose($handle);
-		} 
+		$checksum 		= md5($temp_graphviz_filename);
 		
 		if($export_type == "html"){
 			$img_file 	= "$directory_dot_img" . "$filesystem_path_separator" . "$checksum" . "." . "$graph_default_image_extension";
@@ -163,77 +138,50 @@
 		}
 		
 		if($export_type == "img"){
-			$mapCmd	= "$command_executable_dot -T$graph_default_image_format $dot_file";
+			$mapCmd	= "$command_executable_dot -T$graph_default_image_format $temp_graphviz_filename";
 			header("Content-type: $header");
 			passthru($mapCmd,$ret);
 		} else if($export_type == "html"){
 			header("Content-type: $header");
-			//echo $command_executable_dot;
+			
 			if(!$utility->checkFile("$map_file",$filesystem_age_time)){
 				if(!$utility->checkFile("$img_file",$filesystem_age_time)){
-					$mapCmd	= "$command_executable_dot -Tcmap -o$map_file -T$graph_default_image_format -o$img_file $dot_file";
+					$mapCmd	= "$command_executable_dot -Tcmap -o$map_file -T$graph_default_image_format -o$img_file $temp_graphviz_filename";
+					//echo $mapCmd."<br />";
 					exec($mapCmd,$output,$ret);			
 				}
 			} 
-			//echo $ret;
-			//print_r( $output);
-			//echo "<pre>$mapCmd</pre>";
-			/** Read the contents of the map file */
-			//echo "exists" .file_exists($dot_file) == true;
 			
+			//echo "Return Code: $ret<br />";
+			//echo "Command Output:" . print_r($output). "<br />";
+						
 			if(file_exists($map_file)){
 				$handle 	= fopen("$map_file","rb");
-				$cleanMap 	= "";
+				
+				echo "<center><img src=\"$img_url\" alt=\"Model\" ";
+				echo "class=\"model\" usemap=\"#$img_url\" border=\"0\"></center>\n";
+				echo "<map name=\"$img_url\">\n";
 				
 				while (!feof($handle)) {
-					$cleanMap .= ereg_replace('\\\"','"',fread($handle, 8192));
+					echo ereg_replace('\\\"','"',fread($handle, 4096));
 				}
 				
-				$map_lines = split("\n",$cleanMap);
-				/*$new_lines = array();
-				foreach($map_lines as $index => $line){				
-					if(ereg("href=\".+\"",$line)){
-						$area_parts = explode(" ",$line);
-						$new_parts = array();
-						foreach($area_parts as $area_index => $part){
-							if(ereg("href=\".+\"",$part)){
-								array_push($new_parts,$part . " target=\"_parent\"");
-							} else{
-								array_push($new_parts,$part);
-							}
-						}
-						$new_lines[$index] = "";
-						foreach($new_parts as $new_index => $part){
-							$new_lines[$index] = $new_lines[$index] . $part . " ";
-						}
-					} else {
-						$new_lines[$index] = $line;
-					}
-				}
-				$newMap		= "";
-				foreach($new_lines as $index => $line){	
-					$newMap = $newMap . "$line\n";
-				}
-				*/
-				//print_r(htmlspecialchars($newMap));
-				
+				echo "</map>";		
 				fclose($handle);
 			} else {
 				die("<br />$map_file not found!<br />");
-			}
-			
-			//echo "<!-- Setup done -->";
-			echo "<center><img src=\"$img_url\" alt=\"Model\" ";
-			echo "class=\"model\" usemap=\"#$img_url\" border=\"0\"></center>\n";
-			echo "<map name=\"$img_url\">\n";
-			echo $cleanMap;
-			//echo $newMap;
-			echo "</map>";			
+			}			
 		}
 		
 	} else if($export_type == "dot"){
-		header("Content-type: $header");
-		echo $graph_string;
+		header("Content-type: $header");		
+		$h = fopen($temp_graphviz_filename,"rb");
+		while (!feof($h)) {
+			echo fread($h, 4096);
+		}
+		
+		fclose($h);
+		
 	} else if($export_type == "legend"){
 		header("Content-type: $header");
 		echo createNodeColorLegendTable($node->getNodeCategory());
