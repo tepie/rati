72a1f454643a5f3316398cf4b4a4cd1364d46e9e
tepietrondi@yahoo.com <tepietrondi@yahoo.com@f95b2e77-e138-0410-83cc-19b3da825df8>
2007-10-02 15:07:19 +0000

    added possible ajax for future

--- Include/Javascript.js
+++ Include/Javascript.js
@@ -45,3 +45,51 @@ function loadPage(page,parm){
 	document.write("<br />");
 	window.open(location,'_self');
 }
+
+/** Ajax
+*/
+
+var xmlHttp;
+
+function getXmlHttpObject(){
+	var xmlHttp = null;
+	try {
+		// Firefox, Opera 8.0+, Safari
+		xmlHttp=new XMLHttpRequest();
+	} catch (e) {
+		// Internet Explorer
+		try	{
+			xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");
+		} catch (e)	{
+			try	{
+				xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+			}catch (e)	{
+				alert("Your browser does not support AJAX!");
+				return false;
+			}
+		}
+	}
+	
+	return xmlHttp;
+}
+
+function showHtmlExport(url){
+	alert(url);
+	document.getElementById("image_html_export").innerHTML="Loading...";
+	xmlHttp=GetXmlHttpObject();
+	if (xmlHttp==null){
+		alert ("Your browser does not support AJAX!");
+		return;
+	} 
+	
+	xmlHttp.onreadystatechange=stateChanged;
+	xmlHttp.open("GET",url,true);
+	xmlHttp.send(null);
+}
+
+function stateChanged() { 
+	if (xmlHttp.readyState==4){ 
+		document.getElementById("image_html_export").innerHTML=xmlHttp.responseText;
+	}
+}
+
