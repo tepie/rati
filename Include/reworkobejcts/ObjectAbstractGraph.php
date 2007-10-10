a66cefb77705ded675c3c6d3251b337ba1ffcb2e
tepietrondi@yahoo.com <tepietrondi@yahoo.com@f95b2e77-e138-0410-83cc-19b3da825df8>
2007-10-10 14:34:32 +0000

    mid commit for renaming

--- Include/reworkobejcts/ObjectAbstractGraph.php
+++ Include/reworkobejcts/ObjectAbstractGraph.php
@@ -1,5 +1,6 @@
 <?php 
 
+	
 	abstract class AbstractGraph{
 		
 		private $graph;
@@ -8,38 +9,148 @@
 		private $flag_down;
 		private $limit;
 		private $limit_track;
+		private $nodesVisited;
+		private $local_node_object;
+		private $current_root_node;
 		
-		public function AbstractGraph($runner,$up,$down,$limit){
+		public function AbstractGraph($runner,$up,$down,$limit,$neighbor_limit){
 			$this->query_runner = $runner;
 			$this->flag_up 		= $up;
 			$this->flag_down	= $down;
 			$this->limit 		= $limit;
 			$this->limit_track	= 0;
-			
+			$this->nodesVisited = array();
+			$this->graph		= array();
+			$this->neighbor_limit = $neighbor_limit;
 		}
 		
-		abstract public function walk($node_name,$neighbor_limit);
+		public function walk(AbstractNode $node){
+			/*$node = new NodeObject($this->getQueryRunner(),
+				$node_name,
+				$this->getNeighborLimit());
+			*/	
+			$this->setLocalNodeObject($node);
+			$this->addGraphNode($this->getLocalNodeObject());
+			
+			$neighbors = $this->getLocalNodeObject()->getNeighbors();
+			
+			if(count($neighbors) > 0){
+				$this->track_limit 	= 0;
+				while($this->track_limit < $this->limit){
+					$this->visitUnvisited();
+					$this->track_limit++;
+				}
+			}
+		}
+		
+		abstract public function __toString();
+		
+		public function __destruct(){}
+		
+		private function setLocalNodeObject(AbstractNode $node){
+			$this->local_node_object = $node;
+		}
+		
+		private function getLocalNodeObject(){
+			return $this->local_node_object;
+		}
+		
+		private function getNeighborLimit(){
+			return $this->neighbor_limit;
+		}
 		
 		public function getGraph(){
 			return $this->graph;
 		}
 		
-		public function getUpFlag(){
+		private function getUpFlag(){
 			return $this->flag_up;
 		}
 		
-		public function getDownFlag(){
+		private function getDownFlag(){
 			return $this->flag_down;
 		}
 		
-		public function getLimit(){
+		private function getLimit(){
 			return $this->limit;
 		}
 		
-		public function __destruct(){
-		
+		private function getNodesVisited(){
+			return $this->nodesVisited;
 		}
-	
+		
+		private function getQueryRunner(){
+			return $this->query_runner;
+		}
+		
+		private function inGraph(AbstractNode $nodeObj){
+			if(!array_key_exists($nodeObj->getNodeId(),$this->getGraph())){
+				return True;
+			} else {
+				return False;
+			}
+		}
+		
+		private function inVisited(AbstractNode $nodeObj){
+			if(!array_key_exists($nodeObj->getNodeId(),$this->getNodesVisited())){
+				return True;
+			} else {
+				return False;
+			}
+		}
+		
+		private function addGraphNode(AbstractNode $nodeObj){
+			$result = $this->inGraph($nodeObj);
+			if($result){
+				$id = $nodeObj->getNodeId();
+				$this->graph[$id] = $nodeObj;
+				return true;
+			}
+			return false;
+		}
+		
+		private function addVisitedNode(AbstractNode $nodeObj){
+			$result = $this->inVisited($nodeObj);
+			if($result){
+				$id = $nodeObj->getNodeId();
+				array_push($this->nodesVisited,$id);
+			}
+		}
+		
+		private function visitNode(AbstractNode $nodeObj){
+			$this->addVisitedNode($nodeObj);
+			foreach($nodeObj->getNeighbors() as $node_id => $value){
+				$node_name 	= $this->getLocalNodeObject()->calculateNodeName($node_id);
+				/*$neighborNodeObj 	= new NodeObject($this->getQueryRunner(),
+					$node_name,$this->getLimit());
+				*/
+				
+				$neighborNodeObj = clone $this->getLocalNodeObject();
+				$neighborNodeObj->setNodeName($node_name);
+				
+				$this->addGraphNode($neighborNodeObj);
+				/*if($this->addGraphNode($neighborNodeObj)){
+					$this->graphLink($nodeObj,$node_id,$value);
+				}*/
+			}
+		}
+		
+		private function visitUnvisited(){
+			$graph_keys 	= array_keys($this->getGraph());
+			$visited_keys 	= array_keys($this->getNodesVisited());
+			$unvisited 		= array_diff_key($graph_keys,$visited_keys);
+			
+			foreach($unvisited as $index => $node_id){
+				$node_name 			= $this->getLocalNodeObject()->calculateNodeName($node_id);				
+				$unvisitedNodeObj = clone $this->getLocalNodeObject();
+				$unvisitedNodeObj->setNodeName($node_name);
+				/*$unvisitedNodeObj 	= new NodeObject($this->getQueryRunner(),
+					$node_name,$this->getLimit());
+				*/
+				//$unvisitedNodeObj = $this->getLocalNodeObject();
+				$this->visitNode($unvisitedNodeObj);
+			}
+		}
 	}
 
 ?>
--- Include/reworkobejcts/ObjectCsvGraph.php
+++ Include/reworkobejcts/ObjectCsvGraph.php
@@ -1,33 +1,61 @@
 <?php
 	
 	include_once('ObjectAbstractGraph.php');
-	include_once('SettingsDatabase.php');
-	include_once('SettingsGraph.php');
-	include_once('SQLQueries.php');
+	include_once('ObjectCsvNode.php');
+	//include_once('SettingsDatabase.php');
+	//include_once('SettingsGraph.php');
+	//include_once('SQLQueries.php');
+	
+	include_once('../Database.php');
+	include_once('../SettingsDatabase.php');
 	
 	
 	class GraphCsv extends AbstractGraph{
 	
-		public function GraphCsv($runner,$up,$down){
-			parent::AbstractGraph($runner,$up,$down);
+		public function GraphCsv($runner,$up,$down,$node_limit,$neighbor_limit){
+			parent::AbstractGraph($runner,$up,$down,$node_limit,$neighbor_limit);
 		}
 		
-		public function walk($node_name,$neighbor_limit){
-			global $mysql_database_neighbor_limit;
-			global $directory_dot_graph;
-			
-			// Create a new node object using this query runner and node name
-			if($neighbor_limit != null){
-				//echo $neighbor_limit;
-				$mysql_database_neighbor_limit = $neighbor_limit;
-				//$node 	= new NodeObject($this->query_runner,$node_name,$neighbor_limit);
+		public function __toString(){
+			$text = "";
+			foreach($this->getGraph() as $key => $value){
+				$text = $text . "$key => $value\n";
 			}
+			
+			return $text;
 		}
 	}
 	
-	$x = new GraphCsv(null,true,true,100);
-	echo $x->getUpFlag();
-	echo $x->getLimit();
+	
+	/** Setup the database connection, provide the host, username and password */
+	$db_connection 	= new DbConnectionHandler("$mysql_database_host",
+		"$mysql_database_user",
+		"$mysql_database_passwd"
+	);
+	
+	if(!ISSET($db_connection->link)){
+		$db_connection->setupDbLink();
+		/** The select database results */
+		$x = $db_connection->selectDb("$mysql_database_name");
+	}
+	
+	$node_name 		= "/galileo/Metadata/Categories/category";
+	$query_runner 	= new QueryRunner();
+	$node 			= new NodeBasic($query_runner,10,true,true);
+	$node->setNodeName($node_name);
+	
+	echo "root node: $node\n";
+		
+	$graph 	= new GraphCsv($query_runner,true,true,1,10);
+	
+	$graph->walk($node);
+	
+	echo "graph: $graph\n";
+	
+	if($db_connection->getDbLink() and $x){ $db_connection->closeLink();} 
+	
+	
+	
 
 
 ?>
