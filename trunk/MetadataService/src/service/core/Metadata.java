0a7d0cd743753e680a2955dac8cc77c28e035f26
tepietrondi@yahoo.com <tepietrondi@yahoo.com@f95b2e77-e138-0410-83cc-19b3da825df8>
2007-11-06 01:52:15 +0000

    today's work, this better commit

---
+++ trunk/MetadataService/src/service/core/Metadata.java
@@ -0,0 +1,321 @@
+package service.core;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * A metadata object.
+ * A metadata object tracks annotations and references about itself.
+ * Annotations being textual, and references being pointers to other
+ * metadata. Each metadata object has its own URI. 
+ * @author pietrti
+ *
+ */
+public class Metadata {
+	
+	/**
+	 * Metadata annotation map
+	 */
+	private HashMap<String,HashSet<String>> annotations;
+	/**
+	 * Metadata reference map
+	 */
+	private HashMap<String,HashSet<String>> references;
+	/**
+	 * The URI of this metadata
+	 */
+	private String uri;
+	
+	/**
+	 * Construct a new metadata object
+	 * @param uri The URI of the metadata
+	 */
+	public Metadata(String uri) {
+		this.uri = uri;
+		this.init();
+	}
+	
+	/**
+	 * Print this metadata
+	 */
+	public String toString(){
+		/**
+		 * The return value container
+		 */
+		String returnString = "";
+		/**
+		 * An iterator
+		 */
+		Iterator<String> keyItr;
+		/**
+		 * A set
+		 */
+		Set<String> keys;
+		
+		/**
+		 * Get the annotation kets
+		 */
+		keys = this.annotations.keySet();
+		
+		/**
+		 * Iterate the annotation keys
+		 */
+		keyItr = keys.iterator();
+		while(keyItr.hasNext()){
+			/**
+			 * The current key
+			 */
+			String currentKey = keyItr.next();
+			/**
+			 * The curreent key's annotation values
+			 */
+			Set<String> values = this.annotations.get(currentKey);
+			/**
+			 * An inner-value iterator
+			 * Iterate the values 
+			 */
+			Iterator<String> valueItr = values.iterator();
+			while(valueItr.hasNext()){
+				/**
+				 * The current value 
+				 */
+				String currentValue = valueItr.next();
+				/**
+				 * Add the current key and the value to the return container
+				 */
+				returnString += currentKey + ":" + currentValue + ";";
+			}
+		}
+		
+		/**
+		 * The reference keys
+		 */
+		keys = this.references.keySet();
+		/**
+		 * Iterate the reference keys
+		 * Do the same above
+		 * Can likely refactor or clean this
+		 * duplication of code
+		 */
+		keyItr = keys.iterator();
+		while(keyItr.hasNext()){
+			String currentKey = keyItr.next();
+			Set<String> values = this.references.get(currentKey);
+			Iterator<String> valueItr = values.iterator();
+			while(valueItr.hasNext()){
+				String currentValue = valueItr.next();
+				returnString += currentKey + "->" + currentValue + ";";
+			}
+		}
+		
+		/**
+		 * Check if the return container is empty.
+		 * If it is, just show the URI.
+		 * Otherwise, show the URI with the return
+		 * container. 
+		 */
+		if(returnString.equals("")){
+			return this.getUri();
+		} else {
+			return this.getUri() + "=" + returnString;
+		}
+		
+	}
+	
+	/**
+	 * Get the URI of this  metadata object
+	 * @return String URI name
+	 */
+	public String getUri(){
+		return this.uri;
+	}
+	
+	/**
+	 * Annotate this metadata.
+	 * Annotations are textual, and are non-referencing. They
+	 * indicate no links or constraints, they are documentation 
+	 * like.
+	 * @param rule The name of the annotation rule
+	 * @param value The value of the annotation
+	 */
+	public void annotate(String rule, String value){
+		if(this.hasAnnoValue(rule, value)){
+			// Pass
+			return;
+		} else {
+			if(!this.hasAnnoRule(rule)){
+				this.addAnnoNewRule(rule);
+			} 
+			/**
+			 * Create an overwrite set of annotations
+			 */
+			HashSet<String> overwrite = this.annotations.get(rule);
+			/**
+			 * Add the value to this set
+			 */
+			overwrite.add(value);
+			/**
+			 * Put the new set in the annotation container
+			 */
+			this.annotations.put(rule, overwrite);
+		}
+	}
+	
+	/**
+	 * Remove an annotation from this metadata.
+	 * Since the same annotation can have many values,
+	 * the value of the annotation must be given.
+	 * @param rule The rule of the annotation 
+	 * @param value The value of the annotation
+	 */
+	public void removeAnno(String rule, String value){
+		if(this.hasAnnoValue(rule, value)){
+			HashSet<String> values = this.annotations.get(rule);
+			if(values.contains(value)){
+				values.remove(value);
+			}
+			
+			this.annotations.put(rule, values);
+		}
+	}
+	
+	/**
+	 * Remove a reference from this metadata.
+	 * Since the same reference can have many values,
+	 * the URI to the referencing metadata must be given.
+	 * @param rule The rule of the reference
+	 * @param uriTo The URI of the referencing metadata
+	 */
+	public void removeRef(String rule, String uriTo) {
+		if(this.hasRefValue(rule, uriTo)){
+			HashSet<String> values = this.references.get(rule);
+			if(values.contains(uriTo)){
+				values.remove(uriTo);
+			}
+			
+			this.annotations.put(rule, values);
+		}
+		
+	}
+	
+	/**
+	 * Add a reference to this metadata
+	 * @param rule The rule of the reference
+	 * @param uri The URI to the metadata this reference references
+	 */
+	public void reference(String rule, String uri){
+		if(this.hasRefValue(rule, uri)){
+			// Pass
+			return;
+		} else {
+			if(!this.hasRefRule(rule)){
+				this.addRefNewRule(rule);
+			} 
+			
+			HashSet<String> overwrite = this.references.get(rule);
+			overwrite.add(uri);
+			
+			this.references.put(rule, overwrite);
+		}
+	}
+	
+	/**
+	 * Init this object
+	 */
+	private void init(){
+		this.annotations = new HashMap<String,HashSet<String>>();
+		this.references = new HashMap<String,HashSet<String>>();
+	}
+	
+	/**
+	 * Add a new annotation rule to the annotation map
+	 * @param rule The name of the annotation rule
+	 */
+	private void addAnnoNewRule(String rule){
+		this.annotations.put(rule, new HashSet<String>());
+	}
+	
+	/**
+	 * See if this metadata already contains an annotation rule
+	 * @param rule The name of the annotation rule
+	 * @return True of this metadata contains the rule, false otherwise.
+	 */
+	private boolean hasAnnoRule(String rule){
+		return this.annotations.containsKey(rule);
+	}
+	
+	/**
+	 * See if this metadata has a specific value for a rule.
+	 * A rule can be assigned many times with varying values.
+	 * @param rule The name of the annotation rule
+	 * @param value The value of the annotation
+	 * @return True if this metadata contains the annotation value, false otherwise.
+	 */
+	private boolean hasAnnoValue(String rule,String value){
+		
+		if(this.hasAnnoRule(rule)){
+			HashSet<String> local = this.annotations.get(rule);
+			Iterator<String> localItr = local.iterator();
+			while(localItr.hasNext()){
+				if(localItr.next() == value){
+					return true;
+				} 
+			}
+			return false;
+		} else {
+			return false;
+		}
+		
+	}
+	
+	/**
+	 * Add a new reference rule
+	 * @param rule The name of the reference rule
+	 */
+	private void addRefNewRule(String rule){
+		this.references.put(rule, new HashSet<String>());
+	}
+	
+	/**
+	 * See if this metadata contains a specific reference rule
+	 * @param rule The name of the reference rule
+	 * @return True if the metadata contains this reference rule, false otherwise. 
+	 */
+	private boolean hasRefRule(String rule){
+		return this.references.containsKey(rule);
+	}
+	
+	/**
+	 * See if this metadata has a specific reference value in a rule. 
+	 * Since the rule can be given multiple values. 
+	 * @param rule The name of the reference rule
+	 * @param value The name of the reference value
+	 * @return True if the reference value is contained in the rule set. 
+	 */
+	private boolean hasRefValue(String rule,String value){
+		
+		if(this.hasAnnoRule(rule)){
+			HashSet<String> local = this.references.get(rule);
+			try{
+				Iterator<String> localItr = local.iterator();
+				while(localItr.hasNext()){
+					if(localItr.next() == value){
+						return true;
+					} 
+				}
+			} catch (java.lang.NullPointerException e){
+				return false;
+			}
+			
+			return false;
+		} else {
+			return false;
+		}
+		
+	}
+
+	
+
+}
---
+++ trunk/MetadataService/src/service/core/MetadataService.java
@@ -0,0 +1,176 @@
+package service.core;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+/**
+ * A metadata service
+ * @author pietrti
+ *
+ */
+public class MetadataService {
+
+	/**
+	 * Metdata object container
+	 */
+	private Vector<Metadata> metadata;
+	/**
+	 * Versions of metadata
+	 */
+	private Vector<Metadata> versions;
+	
+	/**
+	 * Construct a new metadata service
+	 */
+	public MetadataService() {
+		this.metadata = new Vector<Metadata>(0);
+		this.versions = new Vector<Metadata>(0);
+	}
+	
+	/**
+	 * Print this service
+	 */
+	public String toString(){
+		String returnString = "";
+		Iterator<Metadata> metadataItr = this.iterator();
+		while(metadataItr.hasNext()){
+			returnString += metadataItr.next().toString() + "\n";
+		}
+		return returnString;
+	}
+	
+	/**
+	 * Get this service's iterator
+	 * @return the iterator of metadata
+	 */
+	public Iterator<Metadata> iterator(){
+		return this.metadata.iterator();
+	}
+	
+	/**
+	 * Get the number of versions for a URI in this service
+	 * @param uri The URI of the metadata to check
+	 * @return integer totaling the number of  versions
+	 */
+	public int versions(String uri){
+		int total = 0;
+		
+		for(int i = 0; i < this.versions.size(); i++){
+			if(this.versions.get(i).getUri().equals(uri)){
+				total ++;
+			}
+		}
+		
+		return total;
+	}
+	
+	/**
+	 * Add a metadata object to this service
+	 * @param uri The URI of the metadata
+	 * @return integer index of the added metadata
+	 */
+	public int add(String uri){
+		int index = this.findMetadataByUri(uri);
+		if( index == -1){
+			Metadata aboutToAdd = new Metadata(uri);
+			this.metadata.add(aboutToAdd);
+			index = this.metadata.indexOf(aboutToAdd);
+		} 
+		
+		this.version(this.metadata.get(index));
+		return index;
+	}
+	
+	/**
+	 * Annotate a metadata object
+	 * @param uri The URI of the metadata
+	 * @param rule The name of the annotation rule
+	 * @param value The value of the annotation rule
+	 */
+	public void annotate(String uri, String rule, String value){
+		int index = this.add(uri);
+		Metadata local = this.metadata.get(index);
+		local.annotate(rule, value);
+		this.metadata.set(index, local);
+		this.version(local);
+	}
+	
+	/**
+	 * Remove an annotation on a metadata object
+	 * @param uri The URI of the metadata
+	 * @param rule The name of the annotation rule
+	 * @param value The value of the annotation rule
+	 */
+	public void deAnnotate(String uri, String rule, String value){
+		int index = this.add(uri);
+		Metadata local = this.metadata.get(index);
+		local.removeAnno(rule,value);
+		this.metadata.set(index, local);
+		this.version(local);
+	}
+	
+	/**
+	 * Reference a metadata object
+	 * @param uri The URI of the metadata
+	 * @param rule The name of the reference rule
+	 * @param uriTo The URI value that this reference points too
+	 */
+	public void reference(String uri, String rule, String uriTo){
+		int index = this.add(uri);
+		@SuppressWarnings("unused")
+		int refIndex = this.add(uriTo);
+		Metadata local = this.metadata.get(index);
+		local.reference(rule, uriTo);
+		this.metadata.set(index, local);
+		this.version(local);
+	}
+	
+	/**
+	 * Remove an reference on a metadata object
+	 * @param uri The URI of the metadata
+	 * @param rule The name of the reference rule
+	 * @param uriTo The URI value that this reference points too
+	 */
+	public void deReference(String uri, String rule, String uriTo){
+		int index = this.add(uri);
+		Metadata local = this.metadata.get(index);
+		local.removeRef(rule, uriTo);
+		this.metadata.set(index, local);
+		this.version(local);
+	}
+	
+	/**
+	 * Get the vector index of a URI by name
+	 * @param uri The URI to look for
+	 * @return Integer index of the URI in the metadata
+	 */
+	private int findMetadataByUri(String uri){
+		int index;
+		if(this.metadata.capacity() > 0 && !this.metadata.isEmpty()){
+			for(index = 0; index < this.metadata.capacity(); index++){
+				try{
+					Metadata current = this.metadata.get(index);
+					if(current.getUri().equals(uri)){
+						return index;
+					}
+				} catch (java.lang.ArrayIndexOutOfBoundsException e){
+					System.err.println("Array Index Out Of Bounds Exception: " + index);
+					System.err.println("Array Index Out Of Bounds Exception: " + this.metadata.capacity());
+					e.printStackTrace();
+					System.exit(-1);
+				}
+				
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * Version a metadata object
+	 * @param metadata
+	 */
+	private void version(Metadata metadata){
+		this.versions.add(metadata);
+	}
+
+}
