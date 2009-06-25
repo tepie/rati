48eca43edce94da21745eb9554d0ee332ddb4aa1
terrence.a.pietrondi <terrence.a.pietrondi@f95b2e77-e138-0410-83cc-19b3da825df8>
2009-06-25 03:33:25 +0000

    re-branding, adding install and example load

--- Include/SettingsPerspectives.php
+++ Include/SettingsPerspectives.php
@@ -31,89 +31,24 @@
 	* 	perspectives for use. 
 	*/
 	
+
 	/**
-	* Ab Initio Application Persepective
+	* Rosetta IBM Commerce & Java Solution Center Persepective
 	*/
 
-	$abinitio_category_prefix			= 	"/abinitio/categories";
-	$abinitio_category_ereg				= 	"\/abinitio\/categories\/";
-	$abi_default_root_node				= 	"$abinitio_category_prefix/Subject Area";
-	$abinitio_category_reference_rule 	= 	"categoryOID";
-	$abinitio_category_unknown			= 	"Unknown";
-	$abinitio_perspective_name			= 	"ab initio";
+	$ibmjcs_category_prefix				= 	"/ibmjcs/categories";
+	$ibmjcs_category_ereg				= 	"\/ibmjcs\/categories\/";
+	$ibmjcs_default_root_node			= 	"$ibmjcs_category_prefix/Aspect";
+	$ibmjcs_category_reference_rule 		= 	"category";
+	$ibmjcs_category_unknown			= 	"Unknown";
+	$ibmjcs_perspective_name			= 	"IBM Commerce & Java Solution Center";
 	
-	$web_app_abi_node_color_map			=   array();
-	$web_app_abi_node_color_map["$abinitio_category_prefix/Subject Area"] 		= "LightCoral";
-	$web_app_abi_node_color_map["$abinitio_category_prefix/Conceptual Entity"] 	= "orange";
-	$web_app_abi_node_color_map["$abinitio_category_prefix/Logical Element"] 	= "yellowgreen";
-	$web_app_abi_node_color_map["$abinitio_category_prefix/File"]                           = "gray";
-	$web_app_abi_node_color_map["$abinitio_category_prefix/Column"] 			= "violet";
-	$web_app_abi_node_color_map["$abinitio_category_prefix/DML"] 				= "cadetblue";
-	$web_app_abi_node_color_map["$abinitio_category_prefix/Graph"] 		= "deeppink";
-	$web_app_abi_node_color_map["$abinitio_category_prefix/category"] 			= $web_app_default_cat_color;
-	
-	
-	//*************************************************************************
-	//*	System Architect Application 
-	//*	Persepective
-	//*************************************************************************
-
-	$sa_category_prefix					= 	"/system architect/categories";
-	$sa_category_ereg					= 	"\/system\ architect\/categories\/";
-	$sa_default_root_node				= 	"$sa_category_prefix/Encyclopedia";
-	$sa_category_reference_rule			= 	"category";
-	$sa_category_unknown				= 	"Unknown";
-	$sa_perspective_name				= 	"system architect";
-	
-	$web_app_sa_node_color_map			=   array();
-	$web_app_sa_node_color_map["/system architect/categories/Encyclopedia"] 	= "tan";
-	$web_app_sa_node_color_map["/system architect/categories/Model"] 			= "LightCoral";
-	$web_app_sa_node_color_map["/system architect/categories/Entity"] 			= "orange";
-	$web_app_sa_node_color_map["/system architect/categories/Attribute"] 		= "yellowgreen";
-	$web_app_sa_node_color_map["/system architect/categories/Data Element"] 	= "cadetblue";
-	$web_app_sa_node_color_map["/system architect/categories/Column"] 			= "blueviolet";
-	$web_app_sa_node_color_map["/system architect/categories/Table"] 			= "plum";
-	$web_app_sa_node_color_map["/system architect/categories/Database"] 		= "seagreen";
-	$web_app_sa_node_color_map["/system architect/categories/category"] 		= $web_app_default_cat_color;
-	
-	//*************************************************************************
-	//*	MDR Application (galileo)
-	//*	Persepective
-	//*************************************************************************
-
-	$mdr_category_prefix			= 	"/galileo/Metadata/Categories";
-	$mdr_category_ereg				= 	"\/galileo\/Metadata\/Categories";
-	$mdr_default_root_node			= 	"$mdr_category_prefix/category";
-	$mdr_category_reference_rule 	= 	"category";
-	$mdr_category_unknown			= 	"Unknown";
-	$mdr_perspective_name			= 	"galileo";
-	
-	$web_app_mdr_node_color_map		=   array();
-	$web_app_mdr_node_color_map["$mdr_category_prefix/Subject Area"] 		= "LightCoral";
-	$web_app_mdr_node_color_map["$mdr_category_prefix/Entity"] 				= "orange";
-	$web_app_mdr_node_color_map["$mdr_category_prefix/Element"] 			= "yellowgreen";
-	$web_app_mdr_node_color_map["$mdr_category_prefix/Column"] 				= "violet";
-	$web_app_mdr_node_color_map["$mdr_category_prefix/Source Structure"]	= "gray";
-	$web_app_mdr_node_color_map["$mdr_category_prefix/File Group"]			= "tan";
-	$web_app_mdr_node_color_map["$mdr_category_prefix/category"] 			= $web_app_default_cat_color;
-	
-	//*************************************************************************
-	//*	Exeros
-	//*	Persepective
-	//*************************************************************************
-
-	$exeros_category_prefix				= 	"/exeros/categories";
-	$exeros_category_ereg				= 	"\/exeros\/categories";
-	$exeros_default_root_node			= 	"$exeros_category_prefix/category";
-	$exeros_category_reference_rule 	= 	"category";
-	$exeros_category_unknown			= 	"Unknown";
-	$exeros_perspective_name			= 	"exeros";
-	
-	$web_app_exeros_node_color_map		=   array();
-	$web_app_exeros_node_color_map["$exeros_category_prefix/dataset"]	= "gray";
-	$web_app_exeros_node_color_map["$exeros_category_prefix/column"] 	= "violet";
-	$web_app_exeros_node_color_map["$exeros_category_prefix/map"] 		= "orange";
-	$web_app_exeros_node_color_map["$exeros_category_prefix/category"] 	= $web_app_default_cat_color;
+	$web_app_ibmjcs_node_color_map			=   array();
+	$web_app_ibmjcs_node_color_map["$ibmjcs_category_prefix/Resource"] 		= "LightCoral";
+	$web_app_ibmjcs_node_color_map["$ibmjcs_category_prefix/Team"] 			= "orange";
+	$web_app_ibmjcs_node_color_map["$ibmjcs_category_prefix/Project"] 		= "yellowgreen";
+	$web_app_ibmjcs_node_color_map["$ibmjcs_category_prefix/Title"]			= "gray";
+	$web_app_ibmjcs_node_color_map["$ibmjcs_category_prefix/category"]		= $web_app_default_cat_color;
 	
 	//*************************************************************************
 	//*	Combined Application 
@@ -123,39 +58,20 @@
 	//*************************************************************************
 	
 	/** A collection of all the perspective names */
-	$perspective_names				= array($abinitio_perspective_name,
-		$sa_perspective_name,
-		$mdr_perspective_name,
-		$exeros_perspective_name
-	);
+	$perspective_names				= array($ibmjcs_perspective_name);
+
 	/** A collection of all the perspective category prefixes */
-	$perspective_category_prefixes 	= array($abinitio_category_prefix,
-		$sa_category_prefix,
-		$mdr_category_prefix,
-		$exeros_category_prefix
-	);
+	$perspective_category_prefixes 	= array($ibmjcs_category_prefix);
+
 	/** A collection of all the perspective category prefix regular expressions */
-	$perspective_category_eregs		= array($abinitio_category_ereg,
-		$sa_category_ereg,
-		$mdr_category_ereg,
-		$exeros_category_ereg
-	);
+	$perspective_category_eregs		= array($ibmjcs_category_ereg);
+
 	/** A collection of all the perspective default root nodes */
-	$perspective_default_root_nodes = array($abi_default_root_node,
-		$sa_default_root_node,
-		$mdr_default_root_node,
-		$exeros_default_root_node
-	);
+	$perspective_default_root_nodes = array($ibmjcs_default_root_node);
+
 	/** A collection of all the perspective category reference rules */
-	$perspective_category_reference_rules = array($abinitio_category_reference_rule,
-		$sa_category_reference_rule,
-		$mdr_category_reference_rule,
-		$exeros_category_reference_rule
-	);
+	$perspective_category_reference_rules = array($ibmjcs_category_reference_rule);
+
 	/** A collection of all the perspective node color maps */
-	$perspective_node_color_maps = array($web_app_abi_node_color_map,
-		$web_app_sa_node_color_map,
-		$web_app_mdr_node_color_map,
-		$web_app_exeros_node_color_map
-	);
+	$perspective_node_color_maps = array($web_app_ibmjcs_node_color_map);
 ?>
