a266d01df2c565ec1eb88dff94fc7bb51111da15
terrence.a.pietrondi <terrence.a.pietrondi@f95b2e77-e138-0410-83cc-19b3da825df8>
2009-06-25 03:33:25 +0000

    re-branding, adding install and example load

--- trunk/Include/SettingsBranding.php
+++ trunk/Include/SettingsBranding.php
@@ -1,30 +1,39 @@
 <?php
-	//$web_app_default_style_sheet	= 	"./$web_app_dir_include_name/style.css";
-	$web_app_default_style_sheet	= 	"./$web_app_dir_include_name/galileo.css";
+	$web_app_default_style_sheet	= 	"./$web_app_dir_include_name/style.css";
+	
 	/** The application logo (not used) */
-	//$web_app_default_logo_image		= 	"./$web_app_dir_include_name/rati.gif";
-	$web_app_default_logo_image		= 	"./$web_app_dir_include_name/GalileoVisualizer.png";
+	$web_app_default_logo_image		= 	"./$web_app_dir_include_name/rati.gif";
+	
 	/** The application author full name */
 	$web_app_author_full_name		= 	"Terrence A Pietrondi";
+	
 	/** The application author short name */
 	$web_app_author_short_name		= 	"terry";
+	
 	/** The application author e-mail address */
-	$web_app_author_email			= 	"terrence_a_pietrondi@keybank.com";
+	$web_app_author_email			= 	"terrence.a.pietrondi@gmail.com";
+	
 	/** The organizational unit that this application is being developed under */
-	$web_app_organizational_dept	= 	"information management";
-	/** The organizational unit main web site */
-	$web_app_organizational_url		= 	"https://spteams.keybank.com/sites/EDA/Galileo/default.aspx";
+	$web_app_organizational_dept	= 	"None";
+	
 	/** The source code of this application */
 	$web_app_source_code_url	 	= 	"http://code.google.com/p/rati/";
+
+	/** The organizational unit main web site */
+	$web_app_organizational_url		= 	$web_app_source_code_url;
+
 	/** The full name of this application */
-	$web_app_name_full				= 	"Galileo Data Visualizer";
+	$web_app_name_full				= 	"relational analysis through images";
+	
 	/** The short name of this application */
-	$web_app_name_abbrv				= 	"Galileo Data Visualizer";
+	$web_app_name_abbrv				= 	"rati";
+	
 	/** The prefix used on page titles in this application */
-	$web_app_page_title_prefix 		= 	"Galileo Data Visualizer";
+	$web_app_page_title_prefix 		= 	"rati";
+	
 	/** The version number of this application */
 	$web_app_version_number			= 	"1.0";
 	
 	$web_app_jump_to_url			= $web_app_organizational_url;
-	$web_app_jump_to_title			= "galileo jump page";
+	$web_app_jump_to_title			= "rati jump page";
 ?>
